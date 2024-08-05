1543fbbabea2d21e2de91ee6c7b3b29a726229ac
infinitech-mosesalcantara <infinitech.mosesalcantara@gmail.com>
2024-08-05 08:43:04 +0800

    reset

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
-    return "Test";
-});
-
 Route::get('/', function () {
     return view('Homepage/index');
 });
