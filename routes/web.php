c49186cd2199d9f95e1b7df09dbb75053460ec53
infinitech-mosesalcantara <infinitech.mosesalcantara@gmail.com>
2024-08-05 08:38:08 +0800

    test commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return "Test";
+});
+
 Route::get('/', function () {
     return view('Homepage/index');
 });
