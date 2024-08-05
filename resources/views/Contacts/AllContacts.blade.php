50f9bed65cd1bf33156b92c4e0493332d389ccf5
gioevora <giolo.evora@gmail.com>
2024-08-05 09:19:48 +0800

    contatcs

--- resources/views/Contacts/AllContacts.blade.php
+++ resources/views/Contacts/AllContacts.blade.php
@@ -18,15 +18,25 @@
     <nav>
         @include('Layout/Navigation')
         <div class="contact-menu">
-            <ul class="">
-                <li class="contact-list"><a href="">Contacts</a></li>
-                <li class="contact-list"><a href="#">All</a></li>
-                <li class="contact-list"><a href="#">Customer</a></li>
-                <li class="contact-list"><a href="#">Suppliers</a></li>
-                <li class="contact-list"><a href="#">Archieved</a></li>
-                <li class="contact-list"><a href="#">Groups</a></li>
-                <li class="contact-list"><a href="#">Smart lists</a></li>
+            <ul class="d-flex justify-content-between">
+                <div class="main-list d-flex">
+                    <li class="contact-header">Contacts</li>
+                    <li class="contact-list"><a href="#">All</a></li>
+                    <li class="contact-list"><a href="#">Customer</a></li>
+                    <li class="contact-list"><a href="#">Suppliers</a></li>
+                    <li class="contact-list"><a href="#">Archieved</a></li>
+                    <li class="contact-list"><a href="#">Groups</a></li>
+                    <li class="contact-list"><a href="#">Smart lists</a></li>
+                </div>
+                <div class="right-list">
+                    <button class="btn btn-success me-4">New Contacts</button>
+                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                </div>
+
             </ul>
+
+
+
         </div>
     </nav>
 
